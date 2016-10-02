2121c19814ff354fb98df9d6d595b006e0effba0
ipero <ipero@live.com>
2016-10-01 22:55:48 -0500

    make updates

--- movies/app.py
+++ movies/app.py
@@ -1,15 +1,15 @@
-import fresh_tomatoes
+import html_template
 import media
 
 toy_story = media.Movie("Toy Story", "A story of the boy and his toys that come to life.",
-        "https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+        "https://s-media-cache-ak0.pinimg.com/originals/ee/9f/21/ee9f21432dac3689d2d3100d2055f151.jpg",
         "https://www.youtube.com/watch?v=4KPTXpQehio")
 
 frozen = media.Movie("Frozen", "When the newly crowned Queen Elsa accidentally uses her "
                      "power to turn things into ice to curse her home in infinite winter, "
                      "her sister, Anna, teams up with a mountain man, his playful reindeer, "
                      "and a snowman to change the weather condition.",
-                     "https://en.wikipedia.org/wiki/Frozen_(2013_film)#/media/File:Frozen_(2013_film)_poster.jpg",
+                     "http://b-i.forbesimg.com/scottmendelson/files/2013/11/Frozen-movie-poster.jpg",
                      "https://www.youtube.com/watch?v=TbQm5doF_Uc")
 #print(frozen.storyline)
 
@@ -21,18 +21,18 @@ adidas_vs_puma = media.Movie("Adidas vs. Puma", "In the 1920s the two brothers A
                              "https://www.youtube.com/watch?v=tyT0VAfOEpQ")
 ratatouille = media.Movie("Ratatouille","A rat who can cook makes an unusual alliance with a "
                           "young kitchen worker at a famous restaurant.",
-                          "https://en.wikipedia.org/wiki/Ratatouille_(film)#/media/File:RatatouillePoster.jpg",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
                           "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
 secret_life_of_pets = media.Movie("Secret Life of pets", "The quiet life of a terrier named Max is upended when "
                                   "his owner takes in Duke, a stray whom Max instantly dislikes.",
-                                  "https://en.wikipedia.org/wiki/The_Secret_Life_of_Pets#/media/File:The_Secret_Life_of_Pets_poster.jpg",
+                                  "https://upload.wikimedia.org/wikipedia/en/6/64/The_Secret_Life_of_Pets_poster.jpg",
                                   "https://www.youtube.com/watch?v=eWI_Jsw9qUs")
 ice_age = media.Movie("Ice Age: Continental Drift", "Manny, Diego, and Sid embark upon another adventure after their "
                       "continent is set adrift. Using an iceberg as a ship, they encounter sea creatures and battle "
                       "pirates as they explore a new world.",
-                      "https://en.wikipedia.org/wiki/Ice_Age:_Continental_Drift#/media/File:Ice_Age_Continental_Drift.jpg",
+                      "http://3.bp.blogspot.com/-4XOPU0FQ5FA/T_W4WGvT5OI/AAAAAAAAEFE/cJxVdrZhHgI/s640/Ice-Age-Continental-Drift-2012-poster.jpg",
                       "https://www.youtube.com/watch?v=xal3sxSSZp0")
 movies= [toy_story, frozen, adidas_vs_puma, ratatouille, secret_life_of_pets, ice_age]
 
-fresh_tomatoes.open_movies_page(movies)
+html_template.open_movies_page(movies)
