deb2bd4917b2e469f2b9d47f878a9b6ce8867dc3
ipero <ipero@live.com>
2016-10-05 22:02:55 -0500

    works

--- count_words/word_counter.py
+++ count_words/word_counter.py
@@ -1,11 +1,18 @@
 def count_words(text,n):
     words_box = text.split()
-    length = len(words_box)
     output = []
-    for i in range(0,length-1):
-        for j in range(i+1, length):
-            if words_box[i] == words_box[j]:
-                output.append(words_box[i])
+    while(len(words_box)>0):
+        word = words_box[0]
+        words_box.remove(words_box[0])
+        instance = 1
+        if(len(words_box)>=2):
+            i = 0
+            while(i<len(words_box)):
+                if(word==words_box[i]):
+                    instance = instance + 1
+                    words_box.remove(words_box[i])
+                i = i + 1
+        output.append([word, instance])
     print output
 
 count_words("Hi, to day is rainy day in Minneapolis and yesterday was rainy day and day one", 3)
