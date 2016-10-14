d500ae6890e5589659afebf12a1a975fbdd2a338
ipero <ipero@live.com>
2016-10-13 23:12:30 -0500

    conditionals

--- booleans/booleans.py
+++ booleans/booleans.py
@@ -38,3 +38,13 @@ print bool_nine
 # Make me true!
 bool_ten = not(2*2+2 != 3*3-3) and 17 % 4 != 4*4
 print bool_ten
+
+
+# Make sure that the_flying_circus(clown,weather) returns True
+def the_flying_circus():
+    if ________:    # Start coding here!
+        # Don't forget to indent
+        # the code inside this block!
+    elif ________:
+        # Keep going here.
+        # You'll want to add the else statement, too!
