3e051ae8d449dd41e054bf1101bf060f8e83c3fe
ipero <ipero@live.com>
2016-10-14 23:18:10 -0500

    one functions calls another

---
+++ funcitons/script.py
@@ -0,0 +1,9 @@
+    # Things you can do with funcitons:
+
+    # call one funcitons in athorer function
+    def first(n):
+        return n + 1
+
+    def second(n):
+        return first(n) + 2
+    print second(30) # it prints 33
