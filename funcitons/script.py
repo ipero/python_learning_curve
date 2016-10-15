5a3c6f1c35668b0f17d3b42fbe5b2baea974994d
ipero <ipero@live.com>
2016-10-14 23:28:36 -0500

    another example

--- funcitons/script.py
+++ funcitons/script.py
@@ -1,9 +1,21 @@
-    # Things you can do with funcitons:
+# Things you can do with funcitons:
 
-    # call one funcitons in athorer function
-    def first(n):
-        return n + 1
+# call one funcitons in athorer function
+def first(n):
+    return n + 1
 
-    def second(n):
-        return first(n) + 2
-    print second(30) # it prints 33
+def second(n):
+    return first(n) + 2
+print second(30) # it prints 33
+
+# same thing here
+def cube(number):
+    return number**3
+
+def by_three(number):
+    if number % 3 == 0:
+        return cube(number)
+    else:
+        return False
+print by_three(9)
+print by_three(5)
