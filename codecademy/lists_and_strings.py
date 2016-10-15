d55fefd92954494cd928dea672c56f81f99576e4
ipero <ipero@live.com>
2016-10-15 14:05:08 -0500

    lists and strings

---
+++ codecademy/lists_and_strings.py
@@ -0,0 +1,14 @@
+# slice strings and lists
+animals = "catdogfrog"
+cat  = animals[:3]   # The first three characters of animals
+dog  =   animals[3:6]            # The fourth through sixth characters
+frog = animals[6:]              # From the seventh character to the end
+print cat, dog, frog
+
+suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
+
+first  = suitcase[0:2]  # The first and second items (index zero and one)
+middle = suitcase[2:4]  # Third and fourth items (index two and three)
+last   = suitcase[4:6]        # The last two items (index four and five)
+print middle
+print last
