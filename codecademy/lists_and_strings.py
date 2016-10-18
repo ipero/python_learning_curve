18eb292d9304fd88afbec4bd510fd2e42fc8ea9c
ipero <ipero@live.com>
2016-10-17 22:22:10 -0500

    Grocery store

--- codecademy/lists_and_strings.py
+++ codecademy/lists_and_strings.py
@@ -29,3 +29,29 @@ for number in start_list: # number gets assigned value of each list item
 
 square_list.sort()
 print square_list
+
+# store checkout shopping list
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total = total + prices[item]
+            stock[item] -=1
+    return total
+print "Your total grocery bill is $%s" % compute_bill(shopping_list)
