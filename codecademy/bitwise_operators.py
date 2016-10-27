b72fb1855dd16e16c9f90ab19895b1db7431e2b3
ipero <ipero@live.com>
2016-10-26 23:08:08 -0500

    print binary numbers

---
+++ codecademy/bitwise_operators.py
@@ -0,0 +1,12 @@
+# In Python, you can write numbers in binary format by starting the number with 0b
+print 0b0,    #0
+print 0b1,    #1
+print 0b10,   #2
+print 0b11,   #3
+print 0b100,  #4
+print 0b101,  #5
+print 0b110,  #6
+print 0b111   #7
+
+print 0b1 + 0b11
+print 0b11 * 0b11
