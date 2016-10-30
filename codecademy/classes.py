d279369a9821363f35522e6bfd8c22a4cd5f89fd
ipero <ipero@live.com>
2016-10-29 23:07:43 -0500

    classes

--- codecademy/classes.py
+++ codecademy/classes.py
@@ -29,3 +29,54 @@ class PartTimeEmployee(Employee):
 
 matt = PartTimeEmployee("Matt")
 print matt.calculate_wage(28)
+
+# get access to overwritten class method
+class Employee(object):
+    """employees class"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+class PartTimeEmployee(Employee):
+    """part-timer class"""
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return self.hours * 12
+    def full_time_wage(self, hours):
+        return super(PartTimeEmployee, self).calculate_wage(hours)
+
+matt = PartTimeEmployee("Matt")
+print matt.calculate_wage(28)
+
+milton = PartTimeEmployee("Milton")
+print milton.full_time_wage(28)
+
+# more classes sample
+class Triangle(object):
+    """Makes triangle"""
+    def __init__(self, angle1, angle2, angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+    number_of_sides = 3
+    def check_angles(self):
+        if self.angle1+self.angle2+self.angle3 == 180:
+            return True
+        else:
+            return False
+
+my_triangle = Triangle(90,30,60)
+print my_triangle.number_of_sides
+print my_triangle.check_angles()
+
+class Equilateral(Triangle):
+    """Makes equilateral"""
+    angle = 60
+    def __init__(self):
+        self.angle1=self.angle
+        self.angle2=self.angle
+        self.angle3=self.angle
+        
