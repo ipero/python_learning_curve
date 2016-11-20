e52f39cad7129aeea6be5403d1d802acc1f13ae6
ipero <ipero@live.com>
2016-11-19 22:49:06 -0600

    start exercise 25

--- python_the_hard_way/ex24.py
+++ python_the_hard_way/ex24.py
@@ -79,16 +79,16 @@ print "We'd have %s beans, %s jars, and %s crabapples." % (secret_formula(start_
 
 sentence = "All god\tthings come to those who weight."
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
-
-print_first_word(words)
-print_last_word(words)
-print_first_word(sorted_words)
-print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
-print sorted_words
-
-print_irst_and_last(sentence)
-
-print_first_a_last_sorted(senence)
+# words = ex25.break_words(sentence)
+# sorted_words = ex25.sort_words(words)
+#
+# print_first_word(words)
+# print_last_word(words)
+# print_first_word(sorted_words)
+# print_last_word(sorted_words)
+# sorted_words = ex25.sort_sentence(sentence)
+# print sorted_words
+#
+# print_irst_and_last(sentence)
+# 
+# print_first_a_last_sorted(senence)
