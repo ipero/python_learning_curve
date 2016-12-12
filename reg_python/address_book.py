dcb565bc7392219542f9e0c070907cc8b4039217
ipero <ipero@live.com>
2016-12-11 23:00:12 -0600

    capture groups

--- reg_python/address_book.py
+++ reg_python/address_book.py
@@ -92,3 +92,13 @@ twitters = re.searcch(r'''
     , string, re.X | re.M)
 
 print contacts.groups()
+
+# ------------------------------------------------------------------------------
+
+string = '''Love, Kenneth: 20
+Chalkley, Andrew: 25
+McFarland, Dave: 10
+Kesten, Joy: 22
+Stewart Pinchback, Pinckney Benton: 18'''
+
+players = re.search(r'(?P<last_name>[\w ]+),\s(?P<first_name>[\w ]+):\s(?P<score>[\d]+)', string, re.M)
