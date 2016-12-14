019051e6694ea93e4a473a3793f224c65d07a861
ipero <ipero@live.com>
2016-12-14 17:16:05 -0600

    start exercise 47

---
+++ python_the_hard_way/projects/ex47_project/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Automated Testing',
+    'author': 'Roman',
+    'url': 'https://github.com/ipero/python_learning_curve/tree/master/python_the_hard_way',
+    'download_url': 'https://github.com/ipero/python_learning_curve/tree/master/python_the_hard_way',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['EX47'],
+    'scripts': [],
+    'name': 'ex47'
+}
+
+setup(**config)
