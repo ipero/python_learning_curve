3dedc4ad4d0d33966e62fba648e80295a8f172ee
ipero <ipero@live.com>
2016-12-13 22:15:29 -0600

    groups capture

---
+++ meetups/node.js/ai_with_nodejs/app.js
@@ -0,0 +1,5 @@
+// Computer Vision
+// microsoft cognitive APIs
+// require project-oxford
+
+// speech recognition LUIS
--- reg_python/address_book.py
+++ reg_python/address_book.py
@@ -95,14 +95,28 @@ twitters = re.search(r'''
 
 # ------------------------------------------------------------------------------
 
-string = '''Love, Kenneth: 20
-Chalkley, Andrew: 25
+string = '''Love, Kenneth: 20 Chalkley, Andrew: 25
 McFarland, Dave: 10
 Kesten, Joy: 22
 Stewart Pinchback, Pinckney Benton: 18'''
 
 players = re.search(r'''
-        (?P<last_name>[\w ]+),
-        \s(?P<first_name>[\w ]+):\s(?P<score>[\d]+)
-    ''', string, re.M)
-print (players)
+    (?P<last_name>[\w ]+),
+    \s(?P<first_name>[\w ]+):\s(?P<score>[\d]+)
+    ''', string, re.X | re.M)
+
+print (players.groupdict())
+
+class Player:
+  def __init__(self, last_name, first_name, score):
+    self.last_name = last_name
+    self.first_name = first_name
+    self.score = score
+
+player = re.compile(r'''
+    (?P<last_name>[\w ]+),
+    \s(?P<first_name>[\w ]+):\s(?P<score>[\d]+)
+    ''', re.X | re.M)
+
+for match in player.finditer(string):
+    print('{first_name} {last_name}, score: {score}'.format(**match.groupdict()))
